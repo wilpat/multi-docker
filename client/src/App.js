fabd699fe86d6eba02b61c5bf8852c4f87113598
William Okafor <wilpat456@gmail.com>
2020-02-13 02:18:46 +0100

    Minor change to test update on AWS

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,10 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Fib Calculator.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Enter a number and reload.</p>
+          
           <Link to="/"> Home </Link>
           <Link to="/otherpage"> Other Page </Link>
         </header>
